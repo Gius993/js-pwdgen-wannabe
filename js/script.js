88dfa587236c21eff647964f737e8baa53eb9128
Gius993 <giuseppevidili9@gmail.com>
2022-05-31 15:47:22 +0200

    push exstra test colore

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const userName = prompt('Dimmi il tuo nome');
 //Cognome utente
 const userLastame = prompt('Dimmi il tuo cognome');
 //Colore preferito
-const userFavoritecolor = prompt('Dimmi il tuo colore preferito');
+const userFavorite = prompt('Dimmi il tuo colore preferito');
 //password
 //Creo il messaggio finale per l'utente
-const userMessage =  userName + userLastame + userFavoritecolor + ('21');
+const userMessage =  userName + userLastame + userFavorite + ('21');
 
 //Password
 document.getElementById('password').innerHTML = userMessage;
